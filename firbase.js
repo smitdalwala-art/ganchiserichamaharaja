0aa7eecc2d07acd64ef2759e52012ee3fd29df5b
smitdalwala-art <smitdalwala@gmail.com>
2026-05-02 20:25:27 +0530

    Add files via upload

---
+++ firbase.js
@@ -0,0 +1,7 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAY5uTqa6oUBvMUtanpzsJvh1gGGoodbDk",
+  authDomain: "ganesh-mandal-51d9b.firebaseapp.com",
+  databaseURL: "https://ganesh-mandal-51d9b-default-rtdb.firebaseio.com",
+  projectId: "ganesh-mandal-51d9b",
+  storageBucket: "ganesh-mandal-51d9b.appspot.com"
+}; 
